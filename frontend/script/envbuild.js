d000f977a184763a384878ae59093859653f5a71
concertypin <55056558+concertypin@users.noreply.github.com>
2025-10-02 19:21:34 +0900

    ci: give the building information on build

---
+++ frontend/script/envbuild.js
@@ -0,0 +1,51 @@
+
+
+/**
+ * It is used in vite.config.ts to set environment variables
+ * @param {Record<string, string|undefined>} env loadEnv(mode, process.cwd(), '')
+ * @returns {Record<string, string>} An object containing environment variables
+ */
+export default function getEnvVar(env) {
+    const GITHUB_REPO_URL = env.REPOSITORY_URL// Netlify build
+        || "https://github.com/" + (env.GITHUB_REPOSITORY || "concertypin/ArisuTalk");
+
+    let versionChannel = 'local';
+    let versionName = 'secret';
+    let versionUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"; // You know this
+
+    if (env.GITHUB_ACTIONS) {
+        const ref = env.GITHUB_REF || ""
+        const sha = env.GITHUB_SHA || '';
+
+        if (ref.startsWith('refs/tags/v')) {
+            const tag = ref.replace('refs/tags/', '');
+            versionChannel = 'prod';
+            versionName = tag.substring(1); // 'v' prefix elemination
+            versionUrl = `${GITHUB_REPO_URL}/tree/${tag}`;
+        }
+        else if (ref.startsWith('refs/tags/dev')) {
+            const tag = ref.replace('refs/tags/', '');
+            versionChannel = 'dev';
+            versionName = tag.substring(3); // 'dev' prefix elemination
+            versionUrl = `${GITHUB_REPO_URL}/tree/${tag}`;
+        }
+        else if (ref === 'refs/heads/main') {
+            versionChannel = 'spark';
+            versionName = sha.substring(0, 7); // 7-character short SHA
+            versionUrl = `${GITHUB_REPO_URL}/commit/${sha}`;
+        }
+    }
+
+    else if (env.NETLIFY) {
+        // Netlify build for PR previews
+        versionChannel = "PR";
+        versionName = env.COMMIT_REF?.substring(0, 7) || "unknown";
+        versionUrl = `${GITHUB_REPO_URL}/commit/${env.COMMIT_REF}`;
+    }
+
+    return {
+        VITE_VERSION_CHANNEL: versionChannel,
+        VITE_VERSION_NAME: versionName,
+        VITE_VERSION_URL: versionUrl,
+    };
+}
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv, UserConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { VitePWA } from "vite-plugin-pwa";
+import getEnvVar from "./script/envbuild";
 
 // Since it distracts debugging via service worker, enable it only on production build
 const prodOnlyPlugin = [
@@ -28,30 +29,41 @@ const prodOnlyPlugin = [
         }
     })
 ]
-export default defineConfig(({ mode }) => ({
-    // import.meta.env.DEV is automatically set by Vite based on the 'mode'.
-    // It is true in development (pnpm dev:fe) and false in production (pnpm build:fe).
-    server: {
-        open: "index.html",
-    },
-    build: {
-        outDir: "dist",
-        sourcemap: "inline",
-        rollupOptions: {
-            output: {
-                manualChunks: {
-                    // 벤더 라이브러리들을 별도 청크로 분리
-                    'vendor-core': ['svelte', 'svelte/internal'],
-                    'vendor-ui': ['lucide-svelte'],
-                    'vendor-utils': ['jszip'],
+export default defineConfig(async ({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    const defines: Record<string, any> = {
+        ...getEnvVar(env)
+    }
+
+    const defineConfigReady = Object.fromEntries(Object.entries(defines).map(([k, v]) => [`import.meta.env.${k}`, JSON.stringify(v)]))
+    let baseConfig: UserConfig = {
+        // import.meta.env.DEV is automatically set by Vite based on the 'mode'.
+        // It is true in development (pnpm dev:fe) and false in production (pnpm build:fe).
+        server: {
+            open: "index.html",
+        },
+        build: {
+            outDir: "dist",
+            sourcemap: "inline",
+            rollupOptions: {
+                output: {
+                    manualChunks: {
+                        // 벤더 라이브러리들을 별도 청크로 분리
+                        'vendor-core': ['svelte', 'svelte/internal'],
+                        'vendor-ui': ['lucide-svelte'],
+                        'vendor-utils': ['jszip'],
+                    }
                 }
             }
-        }
-    },
-    clearScreen: false,
-    publicDir: "static",
-    plugins: [
-        svelte(),
-        ...(mode === 'production' ? prodOnlyPlugin : [])
-    ]
-}));
+        },
+        clearScreen: false,
+        publicDir: "static",
+        plugins: [
+            svelte(),
+            ...(mode === 'production' ? prodOnlyPlugin : [])
+        ],
+        define: defineConfigReady
+    };
+    return baseConfig;
+}
+);
